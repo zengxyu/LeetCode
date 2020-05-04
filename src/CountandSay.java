ddcdb48df5b0b9946a58d76b5909106770a5e997
xiangyu zeng <s6xizeng@uni-bonn.de>
2020-05-04 17:05:07 +0200

    2020-05-04

---
+++ src/CountandSay.java
@@ -0,0 +1,35 @@
+/**
+ * Program: LeetCode
+ * Description: The count-and-say sequence is the sequence of integers with the first five terms as following:
+ * Author: Xiangyu Zeng
+ * Create Time: 2020-05-04 16:22
+ **/
+public class CountandSay {
+    public String countAndSay(int n) {
+        String seq = "1";
+        for (int j = 1; j < n; ++j) {
+            StringBuilder stringBuilder = new StringBuilder();
+            char c = seq.charAt(0);
+            int count = 1;
+            for (int i = 1; i <seq.length();i++){
+                if (seq.charAt(i) == c){
+                    count++;
+                }else {
+                    stringBuilder.append(count);
+                    stringBuilder.append(c);
+                    c = seq.charAt(i);
+                    count = 1;
+                }
+            }
+            stringBuilder.append(count);
+            stringBuilder.append(c);
+            seq = stringBuilder.toString();
+        }
+        return seq;
+    }
+    public static void main(String[] args) {
+        CountandSay countandSay = new CountandSay();
+        String a = countandSay.countAndSay(4);
+        System.out.println(a);
+    }
+}
--- src/SearchInsertPosition.java
+++ src/SearchInsertPosition.java
@@ -1,15 +1,16 @@
 /**
  * Program: LeetCode
- * Description: Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.  You may assume no duplicates in the array.
+ * Description: Given a sorted array and a target value,
+ * return the index if the target is found.
+ * If not, return the index where it would be if it were inserted in order.
+ * You may assume no duplicates in the array.
  * Author: Xiangyu Zeng
  * Create Time: 2020-05-04 16:05
  **/
 public class SearchInsertPosition {
     public int searchInsert(int[] nums, int target) {
         for (int i = 0; i < nums.length; ++i) {
-            if (nums[i] < target) {
-                continue;
-            } else if (nums[i] >= target) {
+            if (nums[i] >= target) {
                 return i;
             }
         }
