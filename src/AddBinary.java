501e3b5cc20eb3b6d870c3feb66c83c7b2538b5d
xiangyu zeng <s6xizeng@uni-bonn.de>
2020-05-06 16:53:38 +0200

    gitignore

--- src/AddBinary.java
+++ src/AddBinary.java
@@ -6,67 +6,42 @@ import java.util.List;
  * Description: Given two binary strings, return their sum (also a binary string).  The input strings are both non-empty and contains only characters 1 or 0.
  * Author: Xiangyu Zeng
  * Create Time: 2020-05-06 15:30
+ * What I got from this question ?
+ * （1）能在list或者stringbuffer后面加入，就在后面加入，最后再reverse都行,
+ * 因为insert或者add(0,x)都会让后面的元素逐个向后移，会花费大量时间
+ * （2）利用好 除 或者 余数
  **/
 public class AddBinary {
     public String addBinary(String a, String b) {
-        List<Integer> result = new ArrayList<Integer>();
+        StringBuilder sb = new StringBuilder();
         int pre = 0;
         int a_index = a.length() - 1;
         int b_index = b.length() - 1;
         while (a_index >= 0 || b_index >= 0) {
-            int a_bit = 48;
-            int b_bit = 48;
+            int sum = pre;
             if (a_index >= 0) {
-                a_bit = a.charAt(a_index);
+                sum += a.charAt(a_index) - '0';
             }
             if (b_index >= 0) {
-                b_bit = b.charAt(b_index);
+                sum += b.charAt(b_index) - '0';
             }
-
-            int sum = a_bit + b_bit;
             // 0==48  1==49
-            if (sum == '0' + '0') {
-                if (pre == 1) {
-                    result.add(0, 1);
-                    pre = 0;
-                } else {
-                    result.add(0, 0);
-                    pre = 0;
-                }
-            } else if (sum == '0' + '1') {
-                if (pre == 1) {
-                    result.add(0, 0);
-                    pre = 1;
-                } else {
-                    result.add(0, 1);
-                    pre = 0;
-                }
-            } else if (sum == '1' + '1') {
-                if (pre == 1) {
-                    result.add(0, 1);
-                    pre = 1;
-                } else {
-                    result.add(0, 0);
-                    pre = 1;
-                }
-            }
+            sb.append(sum % 2);
+            pre = sum / 2;
             a_index--;
             b_index--;
         }
-        if (pre == 1){
-            result.add(0,1);
+        if (pre == 1) {
+            sb.append(1);
         }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < result.size(); i++) {
-            sb.append(result.get(i));
-        }
-        return sb.toString();
+        return sb.reverse().toString();
     }
 
     public static void main(String[] args) {
-        String a = "0";
-        String b = "0";
+        String a = "11";
+        String b = "1";
         AddBinary addBinary = new AddBinary();
-        System.out.println(addBinary.addBinary(a,b));;
+        System.out.println(addBinary.addBinary(a, b));
+        ;
     }
 }
